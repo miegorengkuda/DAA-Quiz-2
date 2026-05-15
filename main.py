9e014570f60245e154bc31f7b64b02ffc97ba281
SandIsEverywhere <2abdurrahman341@gmail.com>
2026-05-15 14:24:27 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import sys
+import logging
+
+def tokenize(text):
+    tokens = []
+    current = ''
+
+    for ch in text.lower():
+        if ch.isalnum():
+            current += ch
+        else:
+            if current:
+                tokens.append(current)
+                current = ''
+
+    if current:
+        tokens.append(current)
+
+    return tokens
+
+def remove_stop_word(tokens):
+    tokens.remove()
+
+def lemmatize():
+    pass
+
+def lcs():
+    pass
+
+def main():
+    if len(sys.argv == 2):
+        path1 = sys.argv[1]
+        path2 = sys.argv[2]
+
+if __name__ == '__main__':
+    main()
